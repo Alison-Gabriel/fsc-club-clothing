3e6d66827b8a626a46d06087ebc7b4fa789ea63e
Alison Gabriel <odevalison@gmail.com>
2025-11-24 19:04:55 -0300

    feat: add Header component on login and signup pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,27 +1,32 @@
 import { FaGoogle } from "react-icons/fa";
 
 import Button from "../components/button";
+import Header from "../components/header";
 import LoginForm from "../components/login-form";
 
 const LoginPage = () => {
   return (
-    <main className="flex h-full w-full items-center justify-center p-8">
-      <section className="w-lg space-y-5">
-        <header className="space-y-5 text-center">
-          <h2 className="text-xl font-bold">Entre com a sua conta</h2>
+    <>
+      <Header />
 
-          <Button icon={FaGoogle}>Entrar com o Google</Button>
+      <main className="flex h-[calc(100vh-72px)] w-full items-center justify-center p-8">
+        <section className="w-lg space-y-5">
+          <header className="space-y-5 text-center">
+            <h2 className="text-xl font-bold">Entre com a sua conta</h2>
 
-          <p className="text-brand-dark-foreground text-sm font-semibold">
-            ou entre com o seu e-mail
-          </p>
-        </header>
+            <Button icon={FaGoogle}>Entrar com o Google</Button>
 
-        <div className="bg-brand-darken-blend h-px w-full" />
+            <p className="text-brand-dark-foreground text-sm font-semibold">
+              ou entre com o seu e-mail
+            </p>
+          </header>
 
-        <LoginForm />
-      </section>
-    </main>
+          <div className="bg-brand-darken-blend h-px w-full" />
+
+          <LoginForm />
+        </section>
+      </main>
+    </>
   );
 };
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,18 +1,23 @@
+import Header from "../components/header";
 import SignupForm from "../components/signup-form";
 
 const SignupPage = () => {
   return (
-    <main className="flex h-full w-full items-center justify-center p-8">
-      <section className="w-lg space-y-5">
-        <header className="space-y-5 text-center">
-          <h2 className="text-xl font-bold">Crie a sua conta</h2>
-        </header>
+    <>
+      <Header />
 
-        <div className="bg-brand-darken-blend h-px w-full" />
+      <main className="flex h-[calc(100vh-72px)] w-full items-center justify-center p-8">
+        <section className="w-lg space-y-5">
+          <header className="space-y-5 text-center">
+            <h2 className="text-xl font-bold">Crie a sua conta</h2>
+          </header>
 
-        <SignupForm />
-      </section>
-    </main>
+          <div className="bg-brand-darken-blend h-px w-full" />
+
+          <SignupForm />
+        </section>
+      </main>
+    </>
   );
 };
 
