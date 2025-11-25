9eee1cc5d9a9f2ba9466bb814e4ec3f3e9b6cc67
Alison Gabriel <odevalison@gmail.com>
2025-11-25 00:37:04 -0300

    feat: apply dynamic className on button icon based on icon passed to him

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,10 @@
-import type { ComponentProps, ElementType, ReactNode } from "react";
+import clsx from "clsx";
+import type {
+  ComponentProps,
+  ElementType,
+  FunctionComponent,
+  ReactNode,
+} from "react";
 
 interface ButtonProps extends ComponentProps<"button"> {
   icon: ElementType;
@@ -6,13 +12,17 @@ interface ButtonProps extends ComponentProps<"button"> {
 }
 
 const Button = ({ icon: Icon, children, type, ...props }: ButtonProps) => {
+  const isLoaderIcon =
+    (Icon as FunctionComponent).displayName === "LoaderCircle";
+
   return (
     <button
       {...props}
       type={type}
-      className="bg-brand-background text-brand-foreground hover:bg-brand-background/95 shadow-brand-darken-blend flex w-full items-center justify-center gap-2 rounded-lg py-2.5 text-sm font-bold shadow-md transition-all"
+      className="bg-brand-background text-brand-foreground hover:bg-brand-background/95 shadow-brand-darken-blend disabled:bg-brand-background/80 flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg py-2.5 text-sm font-bold shadow-md transition-all disabled:cursor-default"
     >
-      <Icon className="size-4" /> {children}
+      <Icon className={clsx("size-4", isLoaderIcon && "animate-spin")} />{" "}
+      {children}
     </button>
   );
 };
