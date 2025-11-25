8c196490b168b56f870ac74cfe27588d7b1d78b3
Alison Gabriel <odevalison@gmail.com>
2025-11-25 04:26:46 -0300

    feat: add class variance authority and change base colors and styles on NavLink component

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -1,21 +1,46 @@
-import clsx from "clsx";
+import { cva, type VariantProps } from "class-variance-authority";
 import type { ReactNode } from "react";
 import { Link, type LinkProps, useLocation } from "react-router";
+import { twMerge } from "tw-merge";
 
-interface NavLinkProps extends LinkProps {
+const navLinkVariants = cva(
+  "flex cursor-pointer items-center rounded-2xl bg-transparent px-4 py-1.5 text-xs font-bold transition-colors hover:bg-violet-500/10 items-center justify-center border border-violet-500",
+  {
+    variants: {
+      mode: {
+        dark: "text-brand-foreground",
+        light: "text-brand-dark-foreground",
+      },
+      variant: {
+        selected: "bg-violet-500 hover:bg-violet-500/70",
+        unselected: "bg-transparent text-violet-500",
+      },
+    },
+    defaultVariants: {
+      mode: "dark",
+      variant: "unselected",
+    },
+  },
+);
+
+interface NavLinkProps extends VariantProps<typeof navLinkVariants>, LinkProps {
   children: ReactNode;
 }
 
-const NavLink = ({ children, to, ...props }: NavLinkProps) => {
-  const { pathname: currentPath } = useLocation();
+const NavLink = ({ children, to, mode, className, ...props }: NavLinkProps) => {
+  const { pathname } = useLocation();
+  const isCurrentPath = pathname === to;
 
   return (
     <Link
       {...props}
       to={to}
-      className={clsx(
-        "text-brand-foreground hover:bg-brand-foreground/10 flex cursor-pointer items-center rounded-2xl px-4 py-2 text-sm font-bold transition-all",
-        currentPath === to && "bg-cyan-500 hover:bg-cyan-500/50",
+      className={twMerge(
+        navLinkVariants({
+          variant: isCurrentPath ? "selected" : "unselected",
+          mode,
+          className,
+        }),
       )}
     >
       {children}
