a5d77bb0a8666bb7503c6e080a2a83b8ed593a71
Alison Gabriel <odevalison@gmail.com>
2025-11-24 21:05:38 -0300

    feat: get categories list from firestore

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -1,46 +1,34 @@
-import { useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
+import { categoryConverter } from "../converters/firestore-converter";
+import { db } from "../lib/firebase";
 import type { Category } from "../types/category";
 import CategoryItem from "./category-item";
 
 const Categories = () => {
-  const [categories] = useState<Category[]>([
-    {
-      id: "1",
-      name: "chapeus",
-      displayName: "Chapéus",
-      imageUrl:
-        "https://fastly.picsum.photos/id/90/1920/1080.jpg?hmac=ByhHZw9aMsbOV_LhjHJseKM1qlEZRn1yuwBq_-obtWo",
-    },
-    {
-      id: "2",
-      name: "tenis",
-      displayName: "Tênis",
-      imageUrl:
-        "https://fastly.picsum.photos/id/740/1920/1080.jpg?hmac=hGzHYL3GUn8gr_0xLP7cSSt9DAUuDYDskOrmukxwAkA",
-    },
-    {
-      id: "3",
-      name: "jaquetas",
-      displayName: "Jaquetas",
-      imageUrl:
-        "https://fastly.picsum.photos/id/506/1920/1080.jpg?hmac=EU57GOR9oWJN8E_wW_xZZ8BrLZ1aMp6TwT8M_Y0h7Qg",
-    },
-    {
-      id: "4",
-      name: "feminio",
-      displayName: "Feminino",
-      imageUrl:
-        "https://fastly.picsum.photos/id/657/1920/1080.jpg?hmac=dV43NJtfU6xvNf-JKj9YN4MGMgn3JrgdiOktQdS5vVM",
-    },
-    {
-      id: "5",
-      name: "masculino",
-      displayName: "Masculino",
-      imageUrl:
-        "https://fastly.picsum.photos/id/757/1920/1080.jpg?hmac=2lExxUlIL9Sr4cR2hPzdBO2zFrCErrqss_XsJHgp5YQ",
-    },
-  ]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const fetchCategoriesFromFirestore = async () => {
+    try {
+      const categoriesSnapshot = await getDocs(
+        collection(db, "categories").withConverter(categoryConverter),
+      );
+      const categoriesFromFirestore: Category[] = [];
+
+      categoriesSnapshot.forEach((category) => {
+        categoriesFromFirestore.push(category.data());
+      });
+
+      setCategories(categoriesFromFirestore);
+    } catch (error) {
+      console.error((error as Error).message);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategoriesFromFirestore();
+  }, []);
 
   return (
     <section className="grid grid-cols-2 gap-4 [&>article:nth-child(3)]:col-span-2">
