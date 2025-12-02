a86c303f1dae3e34dbadae4da6448d828a09d439
Alison Gabriel <odevalison@gmail.com>
2025-12-02 15:29:44 -0300

    feat: add userConverter

--- src/converters/firestore-converter.ts
+++ src/converters/firestore-converter.ts
@@ -5,6 +5,7 @@ import type {
 } from "firebase/firestore";
 
 import type { Category } from "../types/category";
+import type { User } from "../types/user";
 
 export const categoryConverter = {
   fromFirestore: (
@@ -23,5 +24,25 @@ export const categoryConverter = {
   },
   toFirestore: (category: Category): DocumentData => {
     return { ...category };
+  },
+};
+
+export const userConverter = {
+  fromFirestore: (
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions,
+  ): User => {
+    const data = snapshot.data(options);
+
+    return {
+      email: data.email,
+      firstName: data.firstName,
+      id: data.id,
+      lastName: data.lastName,
+      provider: data.provider,
+    };
+  },
+  toFirestore: (user: User): DocumentData => {
+    return { ...user };
   },
 };
