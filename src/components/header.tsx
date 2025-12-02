e2e502fcb9fdc32004ae60c63faca52fe0279629
Alison Gabriel <odevalison@gmail.com>
2025-12-02 15:31:51 -0300

    feat: ensure that header buttons are rendered based on user authentication state

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,16 @@ import { BsCart3 } from "react-icons/bs";
 import { PiSignOutBold } from "react-icons/pi";
 import { Link } from "react-router";
 
+import { useAuth } from "../contexts/auth";
 import { auth } from "../lib/firebase";
 
 const Header = () => {
-  const handleLogOut = () => signOut(auth);
+  const { isUserAuthenticated } = useAuth();
+  const isUserNotAuthenticated = !isUserAuthenticated;
+
+  const handleLogOut = async () => {
+    await signOut(auth);
+  };
 
   return (
     <header className="bg-brand-background text-brand-foreground flex h-16 max-h-16 w-full items-center justify-evenly py-5 shadow md:justify-between md:px-5 md:py-4">
@@ -17,27 +23,32 @@ const Header = () => {
           Explorar
         </Link>
 
-        <Link to="/login" className="font-semibold">
-          Login
-        </Link>
+        {isUserNotAuthenticated && (
+          <>
+            <Link to="/login" className="font-semibold">
+              Login
+            </Link>
+            <Link to="/signup" className="font-semibold">
+              Criar Conta
+            </Link>
+          </>
+        )}
 
-        <Link to="/signup" className="font-semibold">
-          Criar Conta
-        </Link>
+        {isUserAuthenticated && (
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-1">
+              <button className="cursor-pointer">
+                <BsCart3 className="size-6" />
+              </button>
 
-        <div className="flex items-center gap-4">
-          <div className="flex items-center gap-1">
-            <button className="cursor-pointer">
-              <BsCart3 className="size-6" />
+              <span className="font-semibold">5</span>
+            </div>
+
+            <button className="cursor-pointer" onClick={handleLogOut}>
+              <PiSignOutBold className="size-6" />
             </button>
-
-            <span className="font-semibold">5</span>
           </div>
-
-          <button className="cursor-pointer" onClick={handleLogOut}>
-            <PiSignOutBold className="size-6" />
-          </button>
-        </div>
+        )}
       </nav>
     </header>
   );
